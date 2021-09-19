15e8b51b9ce26432e0dfd91d57c8498667722870
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2021-09-19 16:52:57 +0200

    right

--- src/esoterum/world/blocks/binary/LogicGraph.java
+++ src/esoterum/world/blocks/binary/LogicGraph.java
@@ -1,11 +1,16 @@
 package esoterum.world.blocks.binary;
 
 import arc.graphics.Color;
+import arc.math.Mathf;
 import arc.struct.Seq;
 
 public class LogicGraph {
     public Seq<BinaryBlock.BinaryBuild> members;
     public Seq<BinaryBlock.BinaryBuild> IOs;
+    public int signal = 0;
 
     public Color color;
+    public LogicGraph(){
+        color = new Color(Mathf.random(0, 255) / 255f, Mathf.random(0, 255) / 255f, Mathf.random(0, 255) / 255f);
+    }
 }
