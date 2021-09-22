2810a8f2e1f841e6291e4a46226cee0a58e225b9
Matthew Peng <54301439+MEEPofFaith@users.noreply.github.com>
2021-09-22 01:23:29 -0700

    Music Swapping (#16)
    * Music Swapping
    
    * Swap dark music as well
    
    Not gonna bother with boss, the testing chamber doesn't spawn units anyways.
    
    * Cleanup
    
    * Minor change to test chamber
    
    ~~haha random commit unrelated to the pr topic go brrr~~

--- src/esoterum/Esoterum.java
+++ src/esoterum/Esoterum.java
@@ -1,11 +1,52 @@
 package esoterum;
 
+import arc.*;
+import arc.audio.*;
+import arc.struct.*;
 import esoterum.content.*;
+import mindustry.game.EventType.*;
 import mindustry.mod.*;
+import mindustry.mod.Mods.*;
+
+import static mindustry.Vars.*;
 
 public class Esoterum extends Mod{
+    private static final Seq<Music>
+        prevAmbient = new Seq<>(),
+        prevDark = new Seq<>();
+    private boolean lastMapEso;
 
-    public Esoterum(){
+    public Esoterum(){}
+
+    @Override
+    public void init(){
+        if(!headless){
+            LoadedMod eso = mods.locateMod("esoterum");
+            Events.on(WorldLoadEvent.class, e -> {
+                boolean isEso = state.map.mod != null && state.map.mod == eso;
+                if(isEso != lastMapEso){
+                    lastMapEso = !lastMapEso;
+                    if(isEso){
+                        swapMusic(control.sound.ambientMusic, EsoMusic.esoAmbientMusic, prevAmbient);
+                        swapMusic(control.sound.darkMusic, EsoMusic.esoDarkMusic, prevDark);
+                        //Log.info("Swapped to Eso music!");
+                    }else{
+                        swapMusic(control.sound.ambientMusic, prevAmbient, null);
+                        swapMusic(control.sound.darkMusic, prevDark, null);
+                        //Log.info("Swapped to Vanilla music!");
+                    }
+                }
+            });
+        }
+    }
+
+    private void swapMusic(Seq<Music> target, Seq<Music> replacement, Seq<Music> save){
+        if(save != null){
+            save.clear();
+            save.addAll(target);
+        }
+        target.clear();
+        target.addAll(replacement);
     }
 
     @Override
--- src/esoterum/content/EsoMusic.java
+++ src/esoterum/content/EsoMusic.java
@@ -4,11 +4,12 @@ import arc.Core;
 import arc.assets.AssetDescriptor;
 import arc.assets.loaders.MusicLoader;
 import arc.audio.Music;
+import arc.struct.*;
 import mindustry.Vars;
 
 // no more "musics"
 public class EsoMusic {
-    protected static Music loadMusic(String musicName) {
+    protected static Music loadMusic(String musicName, boolean ambient, boolean dark){
         if (!Vars.headless) {
             String name = "music/" + musicName;
             String path = Vars.tree.get(name + ".mp3").exists() ? name + ".mp3" : name + ".ogg";
@@ -18,16 +19,22 @@ public class EsoMusic {
             AssetDescriptor<?> desc = Core.assets.load(path, Music.class, new MusicLoader.MusicParameter(music));
             desc.errored = Throwable::printStackTrace;
 
+            if(ambient) esoAmbientMusic.add(music);
+            if(dark) esoDarkMusic.add(music);
             return music;
         } else {
             return new Music();
         }
     }
+
+    public static Seq<Music>
+        esoAmbientMusic = new Seq<>(),
+        esoDarkMusic = new Seq<>();
+
     public static Music
         Eso1;
 
     public void load() {
-        Eso1 = loadMusic("Eso1");
+        Eso1 = loadMusic("Eso1", true, true);
     }
-
 }
