544f9d47ef8d7d6490d535cf5c65a4c33b47e388
Matthew Peng <54301439+MEEPofFaith@users.noreply.github.com>
2021-09-24 16:59:39 -0700

    Manual Land Effect (#30)
    * Manual Land Effect
    
    * Separate draw time from effect lifetime

---
+++ src/esoterum/content/EsoFx.java
@@ -0,0 +1,12 @@
+package esoterum.content;
+
+import arc.graphics.g2d.*;
+import mindustry.entities.*;
+
+public class EsoFx{
+    public static Effect
+
+    manualLand = new Effect(60f, e -> { //TODO this is a placeholder, Goob replace this with your effect later
+        Fill.circle(e.x, e.y, 2f);
+    });
+}
--- src/esoterum/world/blocks/binary/Manual.java
+++ src/esoterum/world/blocks/binary/Manual.java
@@ -1,45 +1,71 @@
 package esoterum.world.blocks.binary;
 
+import arc.audio.*;
 import arc.graphics.g2d.Draw;
 import arc.math.Mathf;
+import arc.util.*;
 import arc.util.io.Reads;
 import arc.util.io.Writes;
+import esoterum.content.*;
 import esoterum.ui.dialogs.ManualDialog;
-import mindustry.gen.Building;
+import mindustry.entities.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
 import mindustry.type.*;
 import mindustry.world.Block;
 import mindustry.world.meta.BuildVisibility;
 
 public class Manual extends Block{
+    public Effect landEffect = EsoFx.manualLand;
+    public float landTime = -1f;
+    public Sound landSound = Sounds.bang;
+
     ManualDialog manual;
+
     public Manual(String name){
         super(name);
         update = true;
         solid = true;
         destructible = true;
         configurable = true;
+        hasShadow = false;
 
         buildVisibility = BuildVisibility.shown;
         category = Category.logic;
     }
 
     @Override
-    public void init() {
+    public void init(){
         super.init();
         manual = new ManualDialog();
+        if(landTime < 0) landTime = landEffect.lifetime;
     }
 
     public class ManualBuild extends Building{
+        public boolean draw;
         public float drawRotation;
 
         @Override
-        public void placed() {
+        public void created(){
+            super.created();
+            landEffect.at(this);
+            Time.run(landTime, () -> {
+                if(!isValid() || draw) return;
+                landSound.at(this, Mathf.random(0.8f, 1.2f));
+                draw = true;
+            });
+        }
+
+        @Override
+        public void placed(){
             super.placed();
             drawRotation = Mathf.random(-50, 50);
         }
 
         @Override
-        public void draw() {
+        public void draw(){
+            if(!draw) return;
+            Drawf.shadow(region, x, y - 1f, drawRotation);
             Draw.rect(region, x, y, drawRotation);
         }
 
@@ -50,24 +76,28 @@ public class Manual extends Block{
         }
 
         @Override
-        public byte version() {
-            return 1;
+        public void write(Writes write) {
+            super.write(write);
+
+            write.f(drawRotation);
+            write.bool(draw);
         }
 
         @Override
         public void read(Reads read, byte revision) {
             super.read(read, revision);
 
-            if(revision == 1){
+            if(revision >= 1){
                 drawRotation = read.f();
+            }
+            if(revision >= 2){
+                draw = read.bool();
             }
         }
 
         @Override
-        public void write(Writes write) {
-            super.write(write);
-
-            write.f(drawRotation);
+        public byte version() {
+            return 2;
         }
     }
 }
