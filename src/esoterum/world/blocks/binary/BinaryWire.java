e183f3218e70f6bb9bfde29cd8b282c7bd3f3366
Matthew Peng <54301439+MEEPofFaith@users.noreply.github.com>
2021-09-24 16:44:38 -0700

    Better Wire Drawing (#27)
    * Better Wire Drawing
    
    * Front covered by top

--- src/esoterum/world/blocks/binary/BinaryWire.java
+++ src/esoterum/world/blocks/binary/BinaryWire.java
@@ -67,6 +67,16 @@ public class BinaryWire extends BinaryBlock{
         }
 
         @Override
+        public void drawConnections(){
+            for(int i = 1; i < 4; i++){
+                if(connections[i]){
+                    Draw.color(Color.white, Pal.accent, getSignal(nb.get(i), this) ? 1f : 0f);
+                    Draw.rect(connectionRegion, x, y, rotdeg() + 90 * i);
+                }
+            }
+        }
+
+        @Override
         public boolean signal() {
             return getSignal(nb.get(1), this) | getSignal(nb.get(3), this);
         }
