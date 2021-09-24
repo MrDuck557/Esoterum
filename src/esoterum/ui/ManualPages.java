b686c0266ce63ae8631e93f6efa6d2aabcb5074b
Goobrr <ggoobbrr@gmail.com>
2021-09-24 08:39:42 +0700

    RTFM

---
+++ src/esoterum/ui/ManualPages.java
@@ -0,0 +1,46 @@
+package esoterum.ui;
+
+public class ManualPages {
+    public static ManualPage[][] topics = new ManualPage[][]{
+        // signal distribution
+        new ManualPage[]{
+            new ManualPage(t -> {
+                t.addText("behold");
+                t.addImage("esoterum-froge", "Fig 1. Froge");
+                t.addText("i see");
+            }),
+            new ManualPage(t -> {
+                t.addText("behold again");
+                t.addImage("esoterum-froge", "Fig 2. Froge");
+                t.addText("i see");
+            }),
+        },
+        // signal sources
+        new ManualPage[]{
+            new ManualPage(t -> {
+                t.addText("sources topic");
+            }),
+            new ManualPage(t -> {
+                t.addText("i am stupid");
+            }),
+        },
+        // gates
+        new ManualPage[]{
+            new ManualPage(t -> {
+                t.addText("gates topic");
+            }),
+            new ManualPage(t -> {
+                t.addText("i am stupid");
+            }),
+        },
+        // circuits
+        new ManualPage[]{
+            new ManualPage(t -> {
+                t.addText("circuits topic");
+            }),
+            new ManualPage(t -> {
+                t.addText("i am stupid");
+            }),
+        },
+    };
+}
--- src/esoterum/ui/dialogs/ManualDialog.java
+++ src/esoterum/ui/dialogs/ManualDialog.java
@@ -1,21 +1,83 @@
 package esoterum.ui.dialogs;
 
+import arc.scene.ui.ScrollPane;
+import arc.scene.ui.layout.Cell;
 import arc.util.Align;
 import esoterum.ui.ManualPage;
+import esoterum.ui.ManualPages;
+import mindustry.gen.Icon;
 import mindustry.gen.Tex;
+import mindustry.graphics.Pal;
 import mindustry.ui.Styles;
 import mindustry.ui.dialogs.BaseDialog;
 
 public class ManualDialog extends BaseDialog {
+    int currentPage;
+    int currentTopic;
+
     public ManualDialog(){
         super("Esoterum Engineer's Manual");
 
-        addCloseButton();
+        build();
+    }
+
+    public void build(){
+
+        // clear dialog contents
+        cont.clearChildren();
+        buttons.clearChildren();
+
+        // build main table
         cont.table(Tex.button, content -> {
-            new ManualPage(t -> {
-                t.addText("behold");
-                t.addText("i see");
-            }).addContent(content);
+            content.pane(Styles.defaultPane, t -> {
+                ManualPages.topics[currentTopic][currentPage].addContent(t);
+            }).grow();
         }).top().size(600f, 800f).name("content");
+
+        // navigation buttons
+        // topic buttons
+        cont.table(topics -> {
+            // distribution
+            topics.button(Icon.distribution, () -> {
+                currentPage = 0;
+                currentTopic = 0;
+                build();
+            }).tooltip("Signal Distribution");
+            topics.row();
+            topics.button(Icon.production, () -> {
+                currentPage = 0;
+                currentTopic = 1;
+                build();
+            }).tooltip("Signal Sources");
+            topics.row();
+            topics.button(Icon.settings, () -> {
+                currentPage = 0;
+                currentTopic = 2;
+                build();
+            }).tooltip("Logic Gates");
+            topics.row();
+            topics.button(Icon.tree, () -> {
+                currentPage = 0;
+                currentTopic = 3;
+                build();
+            }).tooltip("Logic Circuits");
+        }).top().name("topics");
+
+        cont.row();
+        cont.table(t -> {
+            t.labelWrap((currentPage + 1) + "/" + ManualPages.topics[currentTopic].length)
+                .growX().color(Pal.darkishGray);
+        });
+
+        // page buttons
+        buttons.button("Previous Page", () -> {
+            currentPage--;
+            build();
+        }).visible(() -> currentPage - 1 >= 0);
+        addCloseButton();
+        buttons.button("Next Page", () -> {
+            currentPage++;
+            build();
+        }).visible(() -> currentPage + 1 <= ManualPages.topics[currentTopic].length - 1);;
     }
 }
