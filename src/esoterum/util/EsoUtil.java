2e099bcc6926b7201ff0c8520376432f01e196b6
MEEP of Faith <meepmatthew@gmail.com>
2021-09-27 17:04:11 -0700

    Note pitch calculation

--- src/esoterum/util/EsoUtil.java
+++ src/esoterum/util/EsoUtil.java
@@ -1,11 +1,16 @@
 package esoterum.util;
 
+import arc.math.*;
 import mindustry.gen.*;
 
-public class EsoUtil {
-
-    // relativeTo does not account for building rotation.
+public class EsoUtil{
+    /** relativeTo does not account for building rotation. */
     public static int relativeDirection(Building from, Building to){
         return (4 + from.relativeTo(to) - from.rotation) % 4;
+    }
+
+    /** @return the multiplier for the pitch of a sound to be an amount of semitones higher */
+    public static float notePitch(int semitones){
+        return (float)Math.pow(2, semitones / 12f);
     }
 }
