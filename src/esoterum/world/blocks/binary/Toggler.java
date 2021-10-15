b1dae82e0ce67d0560b635e08b86a19eae4a3ae8
MEEP of Faith <meepmatthew@gmail.com>
2021-10-14 19:08:12 -0700

    Toggler sprites

--- src/esoterum/world/blocks/binary/Toggler.java
+++ src/esoterum/world/blocks/binary/Toggler.java
@@ -1,14 +1,19 @@
 package esoterum.world.blocks.binary;
 
+import arc.*;
 import arc.graphics.*;
 import arc.graphics.g2d.*;
+import arc.math.*;
 import mindustry.graphics.*;
 import mindustry.logic.LAccess;
 
 public class Toggler extends BinaryBlock{
+    public TextureRegion[] states = new TextureRegion[2];
+
     public Toggler(String name){
         super(name);
         rotate = true;
+        drawRot = false;
         rotatedBase = true;
         baseType = 1;
         emits = true;
@@ -16,10 +21,18 @@ public class Toggler extends BinaryBlock{
         inputs = new boolean[]{false, true, true, true};
     }
 
-    public class TogglerBuild extends BinaryBuild {
+    @Override
+    public void load(){
+        super.load();
+        states[0] = Core.atlas.find(name + "-off");
+        states[1] = Core.atlas.find(name + "-on");
+        topRegion = states[0];
+    }
+
+    public class TogglerBuild extends BinaryBuild{
         @Override
-        public void updateSignal(int source) {
-            try {
+        public void updateSignal(int source){
+            try{
                 super.updateSignal(source);
                 signal[4] = getSignal(nb.get(1), this) | getSignal(nb.get(2), this) | getSignal(nb.get(3), this);
                 if(signal[0] != signal[4]){
@@ -27,13 +40,21 @@ public class Toggler extends BinaryBlock{
                     propagateSignal(true, false, false, false);
                 }
                 if(front() != null){
-                    front().control(LAccess.enabled, signal() ? 1d : 0d, 0d, 0d, 0d);
+                    front().control(LAccess.enabled, Mathf.num(signal()), 0d, 0d, 0d);
                 }
-            } catch(Exception e){}
+            }catch(Exception ignored){}
         }
 
         @Override
-        public void drawSelect() {
+        public void draw(){
+            drawBase();
+            drawConnections();
+            Draw.color();
+            Draw.rect(states[Mathf.num(signal())], x, y, (rotate && drawRot) ? rotdeg() : 0f);
+        }
+
+        @Override
+        public void drawSelect(){
             super.drawSelect();
             if(front() != null){
                 Draw.color(signal() ? team.color : Color.white);
